e21d19cfe2b63ae1ece2da44c954534c46e0a806
mourodrigo <rodrigobt20@gmail.com>
2022-06-10 14:11:32 -0300

    Initial Commit

---
+++ github-profile-exampleUITests/github_profile_exampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  github_profile_exampleUITestsLaunchTests.swift
+//  github-profile-exampleUITests
+//
+//  Created by Rodrigo Bueno Tomiosso on 10/06/22.
+//
+
+import XCTest
+
+class github_profile_exampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
